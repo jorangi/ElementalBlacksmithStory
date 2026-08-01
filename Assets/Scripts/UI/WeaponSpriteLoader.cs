c91430d27db59f214225c68f7ac7cc8aa76a7a86
jorangi <shfl3182919@gmail.com>
2026-08-01 17:50:17 +0900

    Initial commit

---
+++ Assets/Scripts/UI/WeaponSpriteLoader.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponSpriteLoader : MonoBehaviour
+{
+    [Header("무기 이미지-Image")]
+    [SerializeField]private Image weaponImage;
+    private SpriteAtlas cachedAtlas;
+    void Start()
+    {
+        
+    }    void Update()
+    {
+        
+    }
+}
